58cb46b88927f1172fff55345dbfaff833c0fba5
Yurii <77137059+xYuriizin@users.noreply.github.com>
2022-09-15 00:41:22 -0300

    Update index.js
    adicionando comentários...

--- handler/index.js
+++ handler/index.js
@@ -19,7 +19,7 @@ module.exports = async (client) => {
   client.on("ready", () => {
     console.log(client.user.username + ' Está online e funcional!') //...
     console.log("Slash Commands | OK!") //avisando no console
-    client.guilds.cache.get("808717459695599636").commands.set(SlashCommands) //comando locais (em um único servidor)
+    client.guilds.cache.get("ID DO SEU SERVIDOR DISCORD").commands.set(SlashCommands) //comando locais (em um único servidor)
     /**
      * caso queira que os comandos seja globais, apage a linha de cima e use:
      * client.application.commands.set(SlashCommands) //lembrando que demora cerca de ~1h para os comandos aparecerem
